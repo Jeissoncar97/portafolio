8f693eb7a0f90f583d033f69629de7ba54ad69c1
Jeisson Cardenas <jeissoncar97@gmail.com>
2024-05-27 03:13:49 -0500

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import Profile from '../assets/Fotojeisson.png'
+import '../Estilos/header.css'
+
+function Header() {
+
+  return (
+    <>
+      <div className='header'>
+        <div className="header_content">
+          <div className="header__logo-container">
+            <div className="header__logo-img-cont">
+              <img src={Profile} alt="" />
+            </div>
+            <span className='header__logo-sub'>
+              JEISSON CARDENAS
+            </span>
+          </div>
+          <div className="header_main">
+            <ul className='header-links'>
+              <li className='header-link'>
+                <a href="#home" className='link'>Inicio</a>
+              </li>
+              <li className='header-link'>
+                <a href="#projects" className='link'>Proyectos</a>
+              </li>
+              <li className='header-link'>
+                <a href="#about">Acerca</a>
+              </li>
+              <li className='header-link'>
+                <a href="#contact">Contacto</a>
+              </li>
+              <li className='header-link modo'>
+                <span className='dia'></span>
+                <span className='noche'></span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
