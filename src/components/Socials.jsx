6be823b6fb67e78a3682711607975a7e1010e5b5
Jeisson Cardenas <jeissoncar97@gmail.com>
2024-05-28 01:46:53 -0500

    aside full

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,16 +1,24 @@
 import '../Estilos/social.css'
+import PropTypes from 'prop-types'
 
 
 function Social(props) {
   return (
-    <div className='img-social'>
+    <span className='img-social'>
       <a target='_blank' href={props.link}>
         <img src={props.img} alt={props.alt} title ={props.title} className={props.className}/>
       </a>
-    </div>
+    </span>
   );
 }
 
+Social.propTypes = {
+  link: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  className: PropTypes.string,
+};
 
 
 export default Social;
