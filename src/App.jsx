98e1b105ab682dcfa37f4a0e2a8b1535c0c04cd0
Jeisson Cardenas <jeissoncar97@gmail.com>
2024-05-28 03:13:53 -0500

    aside full, secction  home in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Social from './components/Socials'
+import './Estilos/section-hero-home.css'
 import LinkedInIcon from './assets/linkedin.svg'
 import Github from './assets/github.svg'
 import Correo from './assets/correo.svg'
+import Copy from './assets/copy.svg'
+
 
 function App() {
+  
+  const [copy, setCopy] = useState("") 
+
+  const handleCopy = (event) =>{
+      setCopy(event.target.value)
+  }
+  const handleSubmit = () =>{
+    navigator.clipboard.writeText(copy)
+  }
   return (
     <>
       <Header />
@@ -25,15 +38,32 @@ function App() {
         <Social 
           img={Correo}
           alt="Jeisson Cardenas Correo"
-          link="mailto:jeissonca98@gmail.com"
+          link="mailto:jeissoncar97@gmail.com"
           title = "Jeisson Cardenas correo"
           className="correo-color"
           />
       </aside>
-      <div>
-        
-
-      </div>
+      <section className='home' id='home'>
+        <div className="hero-home-inf">
+          <h1>Hola, Soy Jeisson Cardenas</h1>
+          <div className='hero-home-description'>
+            <p>Soy estudiante de ingenieria en sistemas, apasionado por el <span>DESARROLLO WEB. </span>Me interesa aprender y aplicar tecnologías modernas para crear aplicaciones eficientes y funcionales. Mi objetivo es mejorar constantemente mis <span>habilidades técnicas</span> y contribuir de manera efectiva en los proyectos en los que participo.</p>
+          </div>
+          <div className='hero-home-email'>
+            <input type="text" 
+            name="email" 
+            id="text" 
+            value="jeissoncar97@gmail.com"
+            onChange={handleCopy} />
+            <button id='btn' >
+              <img src={Copy} alt="Copy" onClick={handleSubmit} />
+            </button>
+          </div>
+          <div className="hero-home-button">
+            <a href="#projects" className='button'>PROYECTOS</a>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
