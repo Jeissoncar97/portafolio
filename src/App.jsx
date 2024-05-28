b8e60bf4b88c438b1169a6cad6782907da4d7814
Jeisson <jeissoncar97@gmail.com>
2024-05-27 22:04:39 -0500

    Add aside

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import Header from './components/Header'
 import Social from './components/Socials'
 import LinkedInIcon from './assets/linkedin.svg'
+import Github from './assets/github.svg'
+import Correo from './assets/correo.svg'
 
 function App() {
   return (
@@ -14,7 +16,24 @@ function App() {
           link="https://www.linkedin.com/in/jeisson-cardenas/"
           title = "Jeisson Cardenas Linkedin"
         />
+        <Social 
+          img={Github}
+          alt="Jeisson Cardenas Github"
+          link="https://www.linkedin.com/in/jeisson-cardenas/"
+          title = "Jeisson Cardenas Github"
+        />
+        <Social 
+          img={Correo}
+          alt="Jeisson Cardenas Correo"
+          link="mailto:jeissonca98@gmail.com"
+          title = "Jeisson Cardenas correo"
+          className="correo-color"
+          />
       </aside>
+      <div>
+        
+
+      </div>
     </>
   )
 }
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,11 +1,12 @@
 import '../Estilos/social.css'
 
+
 function Social(props) {
   return (
     <div className='img-social'>
       <a target='_blank' href={props.link}>
-        <img src={props.img} alt={props.alt} title ={props.title} />
-        </a>
+        <img src={props.img} alt={props.alt} title ={props.title} className={props.className}/>
+      </a>
     </div>
   );
 }
