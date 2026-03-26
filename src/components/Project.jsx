edc367add7188baa1df2e07da00e170d4c5e27b8
Jeisson Cardenas <jeissoncar97@gmail.com>
2026-03-26 00:47:23 -0500

    arreglitos

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,56 +1,44 @@
-import propTypes from 'prop-types'
-import GithubTag from '../assets/githubTag.svg'
-import Link from '../assets/link.svg'
-
-
+import propTypes from "prop-types";
 
 const Project = (props) => {
-
-
-  return (
-    <article className="article-project">
-      <div className="cont-img-project">
-        <img src={props.imgProject} alt={props.alt} title={props.alt} className='img-project'/>
-      </div>
-      <div className='project-content-general'>
-        <h3>{props.nameProject}</h3>
-        <div className='project-content'>
-            <ul className='skills'>
-                <li className='skill'>{props.skill1}</li>
-                <li className='skill'>{props.skill2}</li>
-                <li className='skill'>{props.skill3}</li>
-                <li className='skill'>{props.skill4}</li>
-            </ul>
-            <div className='project-description'>
-                <p>{props.projectDescription}</p>
-            </div>
-            <footer className='project-footer'>
-                <a href={props.linkGithub} className='button' target='_blank'>
-                    <img src={GithubTag} alt="" className='icon'/>
-                    <span>Github</span>
-                </a>
-                  <a href={props.linkPage} className='button' target='_blank'>
-                      <img src={Link} alt="" className='icon'/>
-                      <span>Página</span>
-                  </a>
-            </footer>
-        </div>
-      </div>
-    </article>
-  )
-}
+	return (
+		<article className="article-project">
+			<div className="cont-img-project">
+				<img
+					src={props.imgProject}
+					alt={props.alt}
+					title={props.alt}
+					className="img-project"
+				/>
+			</div>
+			<div className="project-content-general">
+				<h3>{props.nameProject}</h3>
+				<div className="project-content">
+					<ul className="skills">
+						<li className="skill">{props.skill1}</li>
+						<li className="skill">{props.skill2}</li>
+						<li className="skill">{props.skill3}</li>
+						<li className="skill">{props.skill4}</li>
+					</ul>
+					<div className="project-description">
+						<p>{props.projectDescription}</p>
+					</div>
+					<footer className="project-footer"></footer>
+				</div>
+			</div>
+		</article>
+	);
+};
 
 Project.propTypes = {
-    nameProject: propTypes.string.isRequired,
-    imgProject: propTypes.string.isRequired,
-    alt: propTypes.string.isRequired,
-    skill1: propTypes.string.isRequired,
-    skill3: propTypes.string.isRequired,
-    skill2: propTypes.string,
-    skill4: propTypes.string,
-    projectDescription: propTypes.string.isRequired,
-    linkGithub: propTypes.string.isRequired,
-    linkPage: propTypes.string.isRequired,
-}
+	nameProject: propTypes.string.isRequired,
+	imgProject: propTypes.string.isRequired,
+	alt: propTypes.string.isRequired,
+	skill1: propTypes.string.isRequired,
+	skill3: propTypes.string.isRequired,
+	skill2: propTypes.string,
+	skill4: propTypes.string,
+	projectDescription: propTypes.string.isRequired,
+};
 
-export default Project
+export default Project;
---
+++ src/components/blog/blog.jsx
@@ -0,0 +1,25 @@
+function Blog() {
+	return (
+		<div style={{ padding: "20px" }}>
+			<h1>🧠 Mini Blog - Hack The Box</h1>
+			<p>Aquí publico mis writeups y aprendizajes en ciberseguridad.</p>
+
+			<div style={{ marginTop: "20px" }}>
+				<div
+					key={2}
+					style={{
+						border: "1px solid #333",
+						padding: "15px",
+						marginBottom: "15px",
+						borderRadius: "10px",
+					}}
+				>
+					<h2>sdfdsfsdf</h2>
+					<p>sdfsdfsdf</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Blog;
